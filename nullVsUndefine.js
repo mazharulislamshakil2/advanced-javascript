119fcb8d8d013061bf509beea179c18d8def4a54
mazharul islam shakil <mazharulislamshakil2@gmail.com>
2021-02-02 20:49:34 +0600

    done

---
+++ nullVsUndefine.js
@@ -0,0 +1,19 @@
+
+// function add(num1 , num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+// function add(num1 , num2){
+//     console.log(num1 , num2);
+// }
+
+
+// const premik = {name :'rohim', phone :4564 , };
+// console.log(premik.gf);
+function doSomething(x, y){
+
+    console.log(y);
+    
+    }
+    
+    doSomething(32);
