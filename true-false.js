1682a86ffc1dc5d1b0c5f086f97da1be35b41c88
mazharul islam shakil <mazharulislamshakil2@gmail.com>
2021-02-02 20:22:44 +0600

    this is good

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// falsy 
+// false
+// 0
+// ""
+// undefine
+// null 
+// NaN
+//  truely
+// '0' , ' ' , [];
+
+const name= '0 ';
+if( name ){
+   console.log ('this is true');
+}
+else{
+    console.log('this is false')
+}
